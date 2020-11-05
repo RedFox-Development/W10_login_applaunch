bfaae69eb047061ab7df2d33bcac6231e10eeb13
RedFoxFinn <31569997+RedFoxFinn@users.noreply.github.com>
2020-11-05 10:37:04 +0200

    Added applaunch ps1-script

---
+++ w10_login_applaunch.ps1
@@ -0,0 +1,6 @@
+﻿$currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+$deviceName = $env:ComputerName
+
+if (($currentUser -eq 'UserName') -and ($deviceName -eq 'UserDevice')) {
+    Start-Process -FilePath 'C:\PathToExecutableOrShortcut';
+}
